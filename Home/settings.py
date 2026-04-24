293974a6adc6214474f4b8925ad6883dcccd57e8
Jishan <ncnoimul1@gmail.com>
2026-04-25 00:51:08 +0600

    Fix email config

--- Home/settings.py
+++ Home/settings.py
@@ -136,5 +136,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ncnoimul2@gmail.com'
-EMAIL_HOST_PASSWORD = 'owefdfbtafdzoyqz'
+EMAIL_HOST_PASSWORD = 'zyezxfectrauqsce'
 DEFAULT_FROM_EMAIL = 'ncnoimul2@gmail.com'
